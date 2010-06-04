2e189b991ebdde1217216a313d90e7c792c097e0
Kris Wallsmith <kris.wallsmith@gmail.com>
2010-06-05 07:11:30 +0800

    Marked Form class as safe for unescaped output.

--- Bundle.php
+++ Bundle.php
@@ -4,7 +4,12 @@ namespace Bundle\FormBundle;
 
 use Symfony\Foundation\Bundle\Bundle as BaseBundle;
 use Symfony\Components\DependencyInjection\ContainerInterface;
+use Symfony\Components\OutputEscaper\Escaper;
 
 class Bundle extends BaseBundle
 {
+    public function boot(ContainerInterface $container)
+    {
+        Escaper::markClassAsSafe('Bundle\\FormBundle\\Form');
+    }
 }
